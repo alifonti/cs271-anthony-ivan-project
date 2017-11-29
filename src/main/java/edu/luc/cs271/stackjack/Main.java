bccb863f14b7aea5b700fd28635e1c0780a56fec
Anthony LiFonti <alifonti@luc.edu>
2017-11-29 02:45:16 +0000

    added table and basic loop

--- src/main/java/edu/luc/cs271/stackjack/Main.java
+++ src/main/java/edu/luc/cs271/stackjack/Main.java
@@ -3,11 +3,31 @@ import java.util.*;
 public class Main {
     public static void main(String[] args)
     {
+        ArrayList<Player> table = new ArrayList<Player>();
         Deck deck = new Deck();
         Player dealer = new Player("Dealer");
-        Player playerOne = new Player("one");
         
-        while(playerOne.getMoney() > 1) {
+        // Adding players
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a name for Player 1: ");
+        String input = scanner.nextLine();
+        Player playerOne = new Player(input);
+        table.add(playerOne);
+        for(int i = 2; i <= 7; i++) {
+            System.out.println("\nEnter another name to add up to 7 Players (or enter \"N\" when finished)");
+            System.out.print("Name " + i + ": ");
+            String name = scanner.nextLine();
+            if(name.equals("N") || name.equals("n") || name.equals("")) {
+                i = 8;
+                System.out.println();
+            }
+            else {
+                table.add(new Player(input));
+            }
+        }
+        
+        
+        while(checkTableMoney(table)) {
             //Place bets
             playerOne.makeBet();
             
@@ -32,5 +52,14 @@ public class Main {
             playerOne.resetHand();
             dealer.resetHand();
         }
+    }
+    
+    public static boolean checkTableMoney(ArrayList<Player> table) {
+        for(int i = 0; i < table.size(); i++) {
+            if(table.get(i).getMoney() > 0) {
+                return true;
+            }
+        }
+        return false;
     }
 }
