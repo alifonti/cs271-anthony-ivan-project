a4ff029ff1d7d6341d6ca10ff3db0aa28314f26a
Anthony LiFonti <alifonti@luc.edu>
2017-11-29 05:05:16 +0000

    changed numOfDecks from 1 to 6

--- src/main/java/edu/luc/cs271/stackjack/Deck.java
+++ src/main/java/edu/luc/cs271/stackjack/Deck.java
@@ -3,14 +3,17 @@ import java.util.*;
 public class Deck {
     ArrayList<Card> allCards = new ArrayList<>();
     Stack<Card> stackedDeck;
+    private int numOfDecks = 6;
     
     //constructor
     public Deck() {
-        for(int i = 1; i <= 13; i++) {
-            allCards.add(new Card("Spade", i));
-            allCards.add(new Card("Heart", i));
-            allCards.add(new Card("Diamond", i)); 
-            allCards.add(new Card("Club", i));
+        for(int j = 1; j <= numOfDecks; j++) {
+            for(int i = 1; i <= 13; i++) {
+                allCards.add(new Card("Spade", i));
+                allCards.add(new Card("Heart", i));
+                allCards.add(new Card("Diamond", i)); 
+                allCards.add(new Card("Club", i));
+            }
         }
         stackedDeck = shuffleAndStack();
     }
@@ -19,7 +22,7 @@ public class Deck {
         ArrayList<Integer> nums = new ArrayList<>();
         Stack<Card> randomizedCards = new Stack<Card>();
         nums.addAll(drawAtRandom());
-        for(int i = 0; i < 51; i++) {
+        for(int i = 0; i < 51*numOfDecks; i++) {
             randomizedCards.push(allCards.get(nums.get(i)));
         }
         return randomizedCards;
@@ -27,7 +30,7 @@ public class Deck {
     }
     public ArrayList<Integer> drawAtRandom() {
         ArrayList<Integer> numbers = new ArrayList<>();
-        for (int i = 0; i <= 51; i++) {
+        for (int i = 0; i <= 51*numOfDecks; i++) {
             numbers.add(i);
         }
         Collections.shuffle(numbers);
